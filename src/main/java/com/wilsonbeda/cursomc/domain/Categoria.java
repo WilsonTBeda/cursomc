b826883a43b5550cdde38e85a1d15cde422993dd
Wilson Beda <wilsontrovaobeda@gmail.com>
2022-10-29 12:30:09 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/wilsonbeda/cursomc/domain/Categoria.java
+++ src/main/java/com/wilsonbeda/cursomc/domain/Categoria.java
@@ -1,16 +1,22 @@
 package com.wilsonbeda.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
 	public Categoria() {
-		
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -37,10 +43,12 @@ public class Categoria implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
-    // compara os objetos por valor e não posição na memória	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -50,9 +58,12 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	
+
 }
