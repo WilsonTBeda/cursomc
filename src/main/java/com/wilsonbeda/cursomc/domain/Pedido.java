bef0df9db31d8fc8a6804d68d04cfe7956d258db
Wilson Beda <wilsontrovaobeda@gmail.com>
2022-11-03 14:48:18 -0300

    Atualização utilizando somente JsonIgnore

--- src/main/java/com/wilsonbeda/cursomc/domain/Pedido.java
+++ src/main/java/com/wilsonbeda/cursomc/domain/Pedido.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -30,11 +29,11 @@ public class Pedido implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm", timezone = "GMT-3")
 	private Date instante;
 	
-	@JsonManagedReference
+	
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido") // peculiaridade do JPA
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
+	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
